0fdb57b89e5e0b52c66ec0ea41b89896473ac838
Mateo Agudelo Garcia <mateo.agudelo80@eia.edu.co>
2021-10-24 21:44:22 -0500

    feat(basics) : first steps

---
+++ First_Example.py
@@ -0,0 +1,17 @@
+time = {'day_hours':24,'week_days':7}
+print(time['day_hours']*time['week_days'])
+
+###################################################
+'''
+RUN BY COMMANDS
+py -{python_version} file_name.py
+py -3.10 First_Example.py
+
+OPEN SHELL
+py -{python_version}
+py -3.10
+
+TEAR DOWN SHELL
+exit()
+'''
+###################################################
